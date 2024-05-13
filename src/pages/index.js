4cc54feb76e0dae6cdac75b828e45a9b4525091d
David Hype <iamsocialhype@gmail.com>
2024-05-12 19:42:08 -0700

    boilerplate

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+ 
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
